d0d0ec9071a560eaba11b97900ccece861be0926
Thiago Gonzalez Lewin <thiagogonzalezlewin@gmail.com>
2025-04-02 20:45:10 -0300

    2

--- src/main/java/net/thiagog/tutorialmod/TutorialMod.java
+++ src/main/java/net/thiagog/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.thiagog.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.thiagog.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
